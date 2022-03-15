754b3b1a8bca2ba8b7c3e6e55f426b4fb296a7ba
DalexHD <eldalexhd@gmail.com>
2022-03-15 19:55:17 +0000

    feat: fix seg fault if no args.

--- srcs/utils/validation.cpp
+++ srcs/utils/validation.cpp
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 17:06:49 by aborboll          #+#    #+#             */
-/*   Updated: 2022/03/10 10:38:11 by aborboll         ###   ########.fr       */
+/*   Updated: 2022/03/15 19:47:41 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 bool validate_args(int argc, char **argv)
 {
-	int port = std::atoi(argv[1]);
 	if (argc < 2 || argc > 3)
 	{
 		std::cerr << "Usage: ./ircserv [port] [password]" << std::endl;
 		return false;
 	}
-	else if (port <= 0)
+	int port = std::atoll(argv[1]);
+	if (port <= 0)
 	{
 		std::cerr << "Port must be a number" << std::endl;
 		return false;
