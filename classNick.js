1495380ddf531826adcc987bc57da8b9d359e1c6
dalexhd <dalexhd@users.noreply.github.com>
2022-09-19 13:30:43 +0000

    deploy: 18a920bba2eb6ed9b47fca0287912c07d4f76ebb

--- classNick.js
+++ classNick.js
@@ -3,5 +3,6 @@ var classNick =
     [ "Nick", "classNick.html#ab4f5b6f3c56272ae8ba78b1bbfed999b", null ],
     [ "Nick", "classNick.html#ab4f5b6f3c56272ae8ba78b1bbfed999b", null ],
     [ "execute", "classNick.html#ac33be0d8bdeb6e18ec697d47c319fe43", null ],
-    [ "execute", "classNick.html#a2f97c20f8cacaf7bf214b2b0373c2879", null ]
+    [ "execute", "classNick.html#a2f97c20f8cacaf7bf214b2b0373c2879", null ],
+    [ "validate", "classNick.html#a93005d1299cbb80f9f8ad534294fa879", null ]
 ];
--- classNotice.js
+++ classNotice.js
@@ -1,5 +1,6 @@
 var classNotice =
 [
     [ "Notice", "classNotice.html#a38ddbc4640546b325eb42aba16273f35", null ],
-    [ "execute", "classNotice.html#a25c73e1779b69e9e3ccc8dc5e49292b4", null ]
+    [ "execute", "classNotice.html#a25c73e1779b69e9e3ccc8dc5e49292b4", null ],
+    [ "validate", "classNotice.html#a10b00bbff7bc6355ca605a1b326772d5", null ]
 ];
