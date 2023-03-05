c86992b5857de5d9c6ce7e5260ccfbf62caebbcf
Marcos Santos serrano <santosserranomarcos@gmail.com>
2023-03-05 13:50:10 +0100

    parsing file

--- tester/includes/Client.hpp
+++ tester/includes/Client.hpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <unistd.h>
 #include <vector>
+#include <map>
 
 class Client
 {
--- tester/main.cpp
+++ tester/main.cpp
@@ -8,6 +8,8 @@
 
 
 
+std::string host = "127.0.0.1"; // nc -c irc.irc-hispano.org 6667
+std::string port = "6667";
 sem_t           sem1;
 int             count = 0;
 pthread_mutex_t mutex;
@@ -15,10 +17,10 @@ typedef struct s_info
 {
 	int                                num_clients;
 	std::vector<pthread_t>             clientThreads;
-	std::map<std::string, std::string> fileContent;
+	std::vector<std::string>			fileContent;
 } t_info;
 
-void parseFile(t_info *info, std::string filename)
+void parseFile(std::string filename)
 {
 	std::ifstream infile(std::string("./tests/spec/" + filename).c_str());
 	// Parse info and save content into info structure
@@ -28,20 +30,23 @@ void parseFile(t_info *info, std::string filename)
 	}
 
 	// example
-	info->fileContent.insert(std::make_pair("nick1", "JOIN #canal1"));
-	info->fileContent.insert(std::make_pair("nick2", "JOIN #canal2"));
-	info->num_clients = 2;
+	//info->fileContent.insert(std::make_pair("nick1", "JOIN #canal1"));
+	//info->fileContent.insert(std::make_pair("nick2", "JOIN #canal2"));
+	//info->num_clients = 2;
 }
 
-static void *clientConversation(void *)
+static void *clientConversation(void *info)
 {
-	for (int i = 0; i < 5000; i++)
-	{
-		pthread_mutex_lock(&mutex);
-		count++;
-		pthread_mutex_unlock(&mutex);
-	}
+	std::vector<std::string> *a = (std::vector<std::string> *)info;
+	Client *client = new Client(host, port);
+	client->login("FirstClient");
+	std::cout << a[0] << "\n";
+	client->send("JOIN CHANNEL1");
 
+	for (int i = 0; i < 10; i++)
+	{
+
+	}
 	return (0);
 }
 
@@ -53,7 +58,7 @@ void createClients(t_info *info)
 	{
 		pthread_t a;
 		info->clientThreads.push_back(a);
-		pthread_create(&info->clientThreads[i], NULL, *clientConversation, NULL);
+		pthread_create(&info->clientThreads[i], NULL, *clientConversation, &info->fileContent);
 	}
 	for (i = 0; i < info->num_clients; i++)
 		pthread_join(info->clientThreads[i], NULL);
@@ -61,8 +66,7 @@ void createClients(t_info *info)
 
 int main(int argc, char *argv[])
 {
-	std::string host = "127.0.0.1"; // nc -c irc.irc-hispano.org 6667
-	std::string port = "6667";
+
 
 	// pthread_mutex_t	*r_fork;
 
@@ -88,9 +92,10 @@ int main(int argc, char *argv[])
 			std::cout << "Automated Test\n";
 			t_info info;
 			info.num_clients = 2;
-			// sem_init(&sem1, 0, 1);
+			info.fileContent.push_back("JOIN CHAN");
 
-			parseFile(&info, "test1");
+
+			parseFile("test1");
 			createClients(&info);
 			std::cout << count;
 
