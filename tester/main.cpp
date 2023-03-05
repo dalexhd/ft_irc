776861d0c8d80d2fab64722f200143560e866b2c
Marcos Santos serrano <santosserranomarcos@gmail.com>
2023-03-05 20:50:23 +0100

    issues to insert element to map of clients, parsing ...

--- tester/main.cpp
+++ tester/main.cpp
@@ -13,21 +13,57 @@ std::string port = "6667";
 sem_t           sem1;
 int             count = 0;
 pthread_mutex_t mutex;
+
 typedef struct s_info
 {
 	int                                num_clients;
 	std::vector<pthread_t>             clientThreads;
-	std::vector<std::string>			fileContent;
+	std::map<std::string, Client>		clients;
+	std::vector<std::string>			fileContent; //name timestamp command
 } t_info;
 
-void parseFile(std::string filename)
+void parseFile(t_info *info,std::string filename)
 {
 	std::ifstream infile(std::string("./tests/spec/" + filename).c_str());
 	// Parse info and save content into info structure
 	for (std::string line; std::getline(infile, line);)
 	{
+		//vtr.push_back("cornflower");
+
+		std::cout << line << std::endl;
+		std::string delimiter = "-";
+
+		size_t pos = 0;
+		size_t i = 0;
+
+		std::string token;
+		while ((pos = line.find(delimiter)) != std::string::npos) {
+			token = line.substr(0, pos);
+			switch(i){
+				case 1:
+
+					if(info->clients.count(token) > 0)
+					{
+						std::cout << "Already exists " << token << std::endl;
+
+					}
+					else{
+						std::cout << "Creating user " << token << std::endl;
+						Client *client = new Client(host, port);
+						client->login(token);
+						info->clients.insert(std::make_pair( token, client ).second);
+					}
+
+
+			}
+
+			std::cout << pos << token << std::endl;
+			line.erase(0, pos + delimiter.length());
+			i++;
+		}
 		std::cout << line << std::endl;
 	}
+
 
 	// example
 	//info->fileContent.insert(std::make_pair("nick1", "JOIN #canal1"));
@@ -40,8 +76,9 @@ static void *clientConversation(void *info)
 	std::vector<std::string> *a = (std::vector<std::string> *)info;
 	Client *client = new Client(host, port);
 	client->login("FirstClient");
-	std::cout << a[0] << "\n";
-	client->send("JOIN CHANNEL1");
+	std::cout << "Command" << a[0][0] << "\n";
+	client->send(a[0][0]);
+	std::cout << client->reads() << std::endl;
 
 	for (int i = 0; i < 10; i++)
 	{
@@ -92,10 +129,10 @@ int main(int argc, char *argv[])
 			std::cout << "Automated Test\n";
 			t_info info;
 			info.num_clients = 2;
-			info.fileContent.push_back("JOIN CHAN");
+			info.fileContent.push_back("JOIN #CHAN");
 
 
-			parseFile("test1");
+			parseFile(&info,"test1");
 			createClients(&info);
 			std::cout << count;
 
