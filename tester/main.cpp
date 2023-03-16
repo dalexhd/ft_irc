a079b527eed7b548de6978b863ec84c6ca44bdf7
DalexHD <eldalexhd@gmail.com>
2023-03-16 16:10:59 +0000

    feat: update.

--- tester/main.cpp
+++ tester/main.cpp
@@ -112,7 +112,7 @@ int main(void)
 		std::cout << "Automated Test\n";
 		std::vector<std::string> files;
 
-		list_files("./tests/new", files);
+		list_files("./tests/spec", files);
 		for (std::vector<std::string>::const_iterator it = files.begin(); // ITERATE FILES
 			it != files.end(); ++it)
 		{
